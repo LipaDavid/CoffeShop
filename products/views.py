e2e0b7a3a89b6fcf70d54a93a3ae82635e18d7fd
Tu Nombre <LipaDavid>
2026-03-27 10:05:12 +0100

    DB Config

--- products/views.py
+++ products/views.py
@@ -1,7 +1,11 @@
 from django.views import generic
-from .forms import ProductForm
 from django.urls import reverse_lazy
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from products.models import Product
+from .forms import ProductForm
+from .serializers import ProductSerializer
+
 
 # Create your views here.
 
@@ -18,3 +22,12 @@ class ProductListView(generic.ListView):
     model = Product
     template_name = 'products/list_product.html'
     context_object_name = 'products'
+
+class ProductListAPI(APIView):
+    authentication_classes = []
+    permission_classes = []
+    
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
